a0fc4ccf1505e60d49a2baf2b22ff9e451533cac
mahesyasn18 <school.mahesyasn18@gmail.com>
2023-11-29 18:53:37 +0700

    feat: export excel

--- app/Exports/RiwayatPenukaranSampahExport.php
+++ app/Exports/RiwayatPenukaranSampahExport.php
@@ -4,19 +4,32 @@ namespace App\Exports;
 
 use App\Models\PenukaranSampah;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Carbon\Carbon;
 
-class RiwayatPenukaranSampahExport implements FromCollection
+class RiwayatPenukaranSampahExport implements FromCollection, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
     */
     public function collection()
-    {
-        return PenukaranSampah::all();
-    }
+{
+    $penukaranSampahCollection = PenukaranSampah::with("users")->select("user_id", "jumlah_sampah", "jumlah_point", "created_at")->get();
 
-    public function headings(): array
-    {
-        return ['user_id','jumlah_sampah','jumlah_point'];
-    }
+    // Append "kg" to the "jumlah_sampah" values and format the date
+    $penukaranSampahCollection->map(function ($item) {
+        $item->jumlah_sampah .= ' kg';
+        $item->created_at = Carbon::parse($item->created_at)->format('d-m-Y H:i:s');
+        $item->nama = $item->users->name;
+
+        return $item;
+    });
+
+    return $penukaranSampahCollection;
+}
+
+public function headings(): array
+{
+    return ['user_id', 'jumlah_sampah', 'jumlah_point', 'created_at', 'nama']; // Change 'Tanggal' to 'created_at'
+}
 }
